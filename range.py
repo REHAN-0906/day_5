c31cbfb684e05608ef7fcf45a5e3a4dd275ec255
rehan <rr1537460@gmail.com>
2025-07-02 14:37:45 +0530

    set type

---
+++ range.py
@@ -0,0 +1,12 @@
+#single parameter 
+a=range(3)
+print(type(a))
+print(a)
+#double parameter 
+b=range(1,10)
+print(type(b))
+print(b)
+#three parameters
+c=range(1,20,2)
+print(type(c))
+print(c)
---
+++ set.py
@@ -0,0 +1,8 @@
+#homo
+a={2,3,4,5,6}
+print(a)
+print(type(a))
+#hetero
+b={1,10+9j,"rehan",20.5}
+print(b)
+print(type(b))
